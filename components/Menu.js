dda522e21ca4266bbb8ee79f534b36256f914587
Kristian Fulkerson <kristianf451@gmail.com>
2021-08-04 11:49:54 -0700

    initial

--- components/Menu.js
+++ components/Menu.js
@@ -1,14 +1,14 @@
 // This is the data we will be using, study it but don't change anything, yet.
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out"
 ];
-
+//change for first commit
 /* 
   Step 1: Write a component called 'menuMaker' to create a menu like the markup below:
 
